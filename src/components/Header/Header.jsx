60d7da865149caa9591e9bfb5629d101a4ed4d44
Sophie Leyshon <sophieleyshon2@gmail.com>
2024-10-17 22:32:01 -0400

    working on events page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import "./Header.scss";
+import React from "react";
+import { Link } from "react-router-dom";
 import bellTalk from "../../assets/icons/bell-talk.svg";
 import bellHero from "../../assets/images/BellICon.webp";
 const Header = () => {
@@ -9,7 +11,7 @@ const Header = () => {
       </div>
       <div className="header__nav">
         <a className="header__nav-home">Home</a>
-        <a className="header__nav-resources">Events</a>
+        <Link to="/events" className="header__nav-resources">Events</Link>
         <a className="header__nav-support">Support</a>
       </div>
     </header>
--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,5 +1,39 @@
-function EventsPage() {
-  return <div>EventsPage</div>;
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './EventsPage.scss';
+
+const EventsPage = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5050/events")
+      .then((response) => {
+        setEvents(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching events data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Upcoming Events</h1>
+      <div className="events-list">
+        {events.map((event) => (
+          <div className="event-card" key={event.id}>
+            <h2>{event.name}</h2>
+            <p>{event.date} - {event.time}</p>
+            <p>Location: {event.location.venue}, {event.location.city}</p>
+            <p>{event.description}</p>
+            <a href={event.registration_link} target="_blank" rel="noopener noreferrer">
+              Register
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default EventsPage;
