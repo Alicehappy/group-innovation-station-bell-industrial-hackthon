96f35aaf5f2d86cb0b53125aaab852f6b1dd5a2e
Anoshka Jhaveri <anoshkaujhaveri@gmail.com>
2024-10-17 23:33:08 -0700

    finished responsive styling for group page, added links to header logo and get started button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,18 @@
 // Hero.jsx
-import React from 'react';
-import './Hero.scss';
+import React from "react";
+import "./Hero.scss";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-    return (
-        <section className="hero">
-            <h1>Welcome to Bell</h1>
-            <p>Find your events here.</p>
-            <button className="hero__cta">Get Started</button>
-        </section>
-    );
+  return (
+    <section className="hero">
+      <h1>Welcome to Bell</h1>
+      <p>Find your events here.</p>
+      <Link to="/events">
+        <button className="hero__cta">Get Started</button>
+      </Link>
+    </section>
+  );
 };
 
 export default Hero;
