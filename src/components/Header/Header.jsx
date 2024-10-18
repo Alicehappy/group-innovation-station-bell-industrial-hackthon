26bccfdbaaa8fe2a709f399ca368998ed673e2e8
Alice Zou <Alicehappy@users.noreply.github.com>
2024-10-17 21:45:45 -0400

    Add Link to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import "./Header.scss";
 import bellTalk from "../../assets/icons/bell-talk.svg";
 import bellHero from "../../assets/images/BellICon.webp";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="header">
@@ -8,9 +10,15 @@ const Header = () => {
         <img src={bellTalk} alt="logo"></img>
       </div>
       <div className="header__nav">
-        <a className="header__nav-home">Home</a>
-        <a className="header__nav-resources">Events</a>
-        <a className="header__nav-support">Support</a>
+        <Link to="/" className="header__nav-home">
+          Home
+        </Link>
+        <Link to="/events" className="header__nav-resources">
+          Events
+        </Link>
+        <Link to="/therapy" className="header__nav-support">
+          Support
+        </Link>
       </div>
     </header>
   );
