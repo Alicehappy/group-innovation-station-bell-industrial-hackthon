9bc09d7059ee8ff3905fecbacb675553ae257893
Alice Zou <Alicehappy@users.noreply.github.com>
2024-10-18 08:28:09 -0400

    Add avatar url data

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,11 +1,17 @@
 import "./Testimonials.scss";
 import "../../components/Testimonial/Testimonial";
 import Testimonial from "../../components/Testimonial/Testimonial";
+import avatarImageUrls from "../../data/avatar-image-url.json";
 
 function Testimonials({ testimonials }) {
+  const combineArray = testimonials.map((testimonial, index) => ({
+    ...testimonial,
+    avatarUrl: avatarImageUrls[index],
+  }));
+  
   return (
     <ul className="testimonial-list">
-      {testimonials.map((testimonial, index) => {
+      {combineArray.map((testimonial, index) => {
         return <Testimonial key={index} testimonial={testimonial} />;
       })}
     </ul>
